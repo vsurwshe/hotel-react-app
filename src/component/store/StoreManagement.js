caa5fce81b78418d06c8acfd6d52a26a40a99e01
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-10-01 17:24:00 +0530

    0.1.1: start the implement the store

--- src/component/store/StoreManagement.js
+++ src/component/store/StoreManagement.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Loader from '../utilites/Loader';
 
 class StoreManagement extends Component {
-    state = {  }
+    state = { 
+        loadStoreItem:false,
+        fromAction:"",
+        storeData:[],
+        operation:""
+    }
+
+    handelLoadStoreItem=()=>{this.setState({ loadStoreItem: !this.state.loadStoreItem})}
+    
+    handelFromAction=(operation, fromAction)=>{this.setState({fromAction: !this.state.fromAction, operation})}
+
+
     render() { 
-        return <h2>Store Management</h2>;
+        const { fromAction }= this.state
+        return fromAction ? this.loadStoreForm(): this.loadStoreTable();
+    }
+
+    loadStoreForm=()=>{
+        return <h2>Store From</h2>
+    }
+
+    loadStoreTable=()=>{
+        const { loadStoreItem }=this.state
+        return loadStoreItem ? <Loader /> : this.loadingStoreTable()
+    }
+
+    loadingStoreTable=()=>{
+        return <h2>Store Table</h2>
     }
 }
- 
-export default StoreManagement;
+
+const mapStateToProps=state=>{return state}
+export default connect(mapStateToProps)(StoreManagement);
